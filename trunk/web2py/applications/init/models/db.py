b3bc0e05e1c873d8cd1dc110aaed629c5bdd53e3
zrx550 <zrx550@8e2592ca-dda9-11de-95c9-073ad895b44c>
2009-12-01 08:03:28 +0000

    initialize database automaticly

--- trunk/web2py/applications/init/models/db.py
+++ trunk/web2py/applications/init/models/db.py
@@ -6,6 +6,11 @@ except:
     session.connect(request,response,db=db )
 
 import datetime
+db.define_table('blog_info',
+    Field('name', required=True),
+    Field('title'),
+    Field('description'),
+    Field('keywords'))
 
 db.define_table('posts',
     Field('post_title', required=True),
@@ -57,3 +62,64 @@ link_labels = {
 cat_labels = {
     'category_name':'Name'
 }
+
+def database_init():
+    """
+    """
+    #print db().select(db.blog_info.ALL)
+    if not db().select(db.blog_info.ALL):
+        db.blog_info.insert(
+                        name='[web2py] PyPress',
+                        description='Just another PyPress weblog',
+                        title='PyPress - a web2py powered weblog',
+                        keywords='web2py, Gluon, Python, Enterprise, Web, Framework, PyPress')
+
+        db.categories.insert(
+                        category_name='uncategorized')
+        db.categories.insert(
+                        category_name='news')
+                        
+        cats=db().select(db.categories.ALL)
+
+        db.posts.insert(
+                        post_title='Hello world!', 
+                        post_text='Welcome to PyPress. This is your first post. Edit or delete it, then start blogging!',
+                        post_type='post',
+                        post_category=cats[0].id)
+        db.posts.insert(
+                        post_title='Welcome to PyPress', 
+                        post_text='This is the Python version of WordPress. Enjoy.',
+                        post_type='post',
+                        post_category=cats[1].id)
+        
+        posts=db().select(db.posts.ALL)
+        
+        db.comments.insert(
+                        post_id=posts[0].id,
+                        comment_author='Richard',
+                        comment_author_email='zrx550@gmail.com',
+                        comment_text="Hi, this is a comment. To delete a comment, just log in and view the post's comments. There you will have the option to edit or delete them.")
+        db.comments.insert(
+                        post_id=posts[0].id,
+                        comment_author='Mr PyPress',
+                        comment_author_email='zrx550@gmail.com',
+                        comment_text='Another comment')
+        db.comments.insert(
+                        post_id=posts[1].id,
+                        comment_author='Richard',
+                        comment_author_web='http://www.google.com',
+                        comment_author_email='zrx550@gmail.com',
+                        comment_text='This is a comment')
+        
+        db.links.insert(
+                        link_title='web2py',
+                        link_url='http://www.web2py.com')
+        db.links.insert(
+                        link_title='WordPressClone',
+                        link_url='http://www.web2py.com/appliances/default/show/36')
+        db.links.insert(
+                        link_title='zrx550.cn',
+                        link_url='http://zrx550.cn')
+
+
+database_init()
