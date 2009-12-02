6168e6e722b8e31c12b9a34ca0038adb3b14fc1d
zrx550 <zrx550@8e2592ca-dda9-11de-95c9-073ad895b44c>
2009-12-02 09:42:55 +0000

    Login function is finished

--- web2py/applications/init/models/_helper_html.py
+++ web2py/applications/init/models/_helper_html.py
@@ -1,2 +1,30 @@
-def URL_user_login():
-    return URL(r=request, c='default', f='login')
+ 
+def get_post_author(author_id):
+    #print author_id
+    user = db(db.users.id==author_id).select()[0]
+    return XML(db(db.users.id == author_id).select()[0].alias)
+
+def get_comment_count(post_id):
+    com_count=len(db(db.comments.post_id==post_id).select(db.comments.ALL))
+    if com_count == 0:
+        com_text="No Comments &raquo;"
+    elif com_count == 1:
+        com_text="1 Comment &raquo;"
+    else:
+        com_text="%s Comments &raquo;" % com_count
+    pass
+    com_link = "<a href='/%(app)s/default/post/%(id)s#comments'>%(text)s</a>" % {'app':request.application, 'id':post_id, 'text':com_text}
+    return XML(com_link)
+
+def get_post_cats(post_id):
+    posts=db(db.posts.id == post_id).select(db.posts.ALL)
+    cats=db(db.categories.id == posts[0].post_category).select(db.categories.ALL)#[0]
+    items=[]
+    for cat in cats:
+        item="<a href='/%(app)s/default/category/%(cat)s'>%(name)s</a>" % {'app':request.application, 'cat':cat.category_name, 'name':cat.category_name}
+        items.append(item)
+    pass
+    cat_list=", ".join(items)
+    return XML(cat_list)
+
+ 
